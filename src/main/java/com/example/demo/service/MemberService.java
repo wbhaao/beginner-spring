90ec5723908663229881fa413ea0280b6117de08
wbhaao <a31620504@gmail.com>
2023-08-26 17:33:57 +0900

    CRUD

--- src/main/java/com/example/demo/service/MemberService.java
+++ src/main/java/com/example/demo/service/MemberService.java
@@ -1,4 +1,37 @@
 package com.example.demo.service;
 
+import com.example.demo.domain.Member;
+import com.example.demo.repogitory.MemberRepogitory;
+import com.example.demo.repogitory.MemoryMemberRepogitory;
+
+import java.util.List;
+import java.util.Optional;
+
 public class MemberService {
+    private final MemberRepogitory memberRepogitory = new MemoryMemberRepogitory();
+    /*
+    * 회원 가입
+    * */
+    public Long join(Member member){
+        // 같은 이름이 있는 중복 회원은 안됨
+        validateDuplicateMember(member);
+
+        memberRepogitory.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+        memberRepogitory.findByName(member.getName())
+            .ifPresent(m -> {
+                throw new IllegalStateException("이미 존재하는 회원입니다.");
+        });
+    }
+    // 전체 회원 조회
+    public List<Member> findMembers() {
+        return memberRepogitory.findAll();
+    }
+
+    public Optional<Member> findOne(Long memberId) {
+        return memberRepogitory.findById(memberId);
+    }
 }
